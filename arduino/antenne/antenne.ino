9ffa4d0b5f7034f748abd3d5e74ff0a011f62dcf
sner123 <106237971+sner123@users.noreply.github.com>
2024-06-04 12:57:32 +0200

    (ongetest) motor code toegevoegd

--- arduino/antenne/antenne.ino
+++ arduino/antenne/antenne.ino
@@ -6,8 +6,8 @@
 #include <Arduino.h>
 
 #include <SPI.h>
-#include <nRF24L01.h>   // to handle this particular modem driver
-#include "RF24.h"       // the library which helps us to control the radio modem
+#include <nRF24L01.h>  // to handle this particular modem driver
+#include "RF24.h"      // the library which helps us to control the radio modem
 
 #define RF24_PAYLOAD_SIZE 32
 #define AAAD_ARO 1
@@ -15,20 +15,26 @@
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10, which are CE & CSN pins  */
 RF24 radio(9, 10);
-const uint8_t rf24_channel[] = {1,26,51,76,101}; // Radio channels set depending on satellite number
+const uint8_t rf24_channel[] = { 1, 26, 51, 76, 101 };                                                            // Radio channels set depending on satellite number
 const uint64_t addresses[] = { 0x4141414430LL, 0x4141414431LL, 0x4141414432LL, 0x4141414433LL, 0x4141414434LL };  //with radioNumber set to zero, the tx pipe will be 'AAAD0', which is basically HEX'4141414430', which is remote DESTINATION address for our transmitted data. The rx pipe code is the local receive address, which is what the remote device needs to set for the remote devices 'tx' pipe code.
 uint8_t txData[RF24_PAYLOAD_SIZE];
 uint8_t rxData[RF24_PAYLOAD_SIZE];
 
+const int stepPin = 6;                                     // PUL+ pin
+const int dirPin = 8;                                      // DIR+ pin
+const int stepsPerRevolution = 5;                          // Afhankelijk van je motor (meestal 200 voor een 1.8° motor)
+const int rpm = 60;                                        // Aantal omwentelingen per minuut
+const int stepDelay = 60000 / (stepsPerRevolution * rpm);  // Bereken de vertraging per stap
+
 // Timing configuration
-unsigned long previousMillis = 0;     // will store last time LED was updated
+unsigned long previousMillis = 0;  // will store last time LED was updated
 unsigned long currentMillis;
-unsigned long sampleTime = 5000;   // milliseconds of on-time
+unsigned long sampleTime = 5000;  // milliseconds of on-time
 
 // int to hex converter
 void printHex2(unsigned v) {
-    Serial.print("0123456789ABCDEF"[v>>4]);
-    Serial.print("0123456789ABCDEF"[v&0xF]);
+  Serial.print("0123456789ABCDEF"[v >> 4]);
+  Serial.print("0123456789ABCDEF"[v & 0xF]);
 }
 
 void setup() {
@@ -48,37 +54,41 @@ void setup() {
 
   // Start the radio listening for data
   radio.startListening();
+
+  // Zet de pinnen als uitgang
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
 }
 
 void loop() {
   // check to see if it's time to change the state of the LED
   currentMillis = millis();
 
-  if(currentMillis - previousMillis >= sampleTime) {
-   
-   
-    unsigned long timeStamp = millis()/1000;
+  if (currentMillis - previousMillis >= sampleTime) {
+
+
+    unsigned long timeStamp = millis() / 1000;
     uint8_t cursor = 0;
 
 
 
-  // test sensorisch meten
-  float AcsValue = 0.0, Samples = 0.0, AvgAcs = 0.0, AcsValueF = 0.0;
-    
-    for (int x = 0; x < 150; x++) { // Verzamel 150 samples
-        AcsValue = analogRead(A0); // Lees de huidige sensorwaarde
-        Samples += AcsValue;  // Voeg samples samen
-        delay(3); // Laat ADC tot rust komen voor de volgende sample (3 ms)
+    // test sensorisch meten
+    float AcsValue = 0.0, Samples = 0.0, AvgAcs = 0.0, AcsValueF = 0.0;
+
+    for (int x = 0; x < 150; x++) {  // Verzamel 150 samples
+      AcsValue = analogRead(A0);     // Lees de huidige sensorwaarde
+      Samples += AcsValue;           // Voeg samples samen
+      delay(3);                      // Laat ADC tot rust komen voor de volgende sample (3 ms)
     }
-    
-    AvgAcs = Samples / 150.0; // Bereken het gemiddelde van de samples
-    
+
+    AvgAcs = Samples / 150.0;  // Bereken het gemiddelde van de samples
+
     // Converteer de gemiddelde ADC waarde naar spanning (0-5V)
     float Voltage = AvgAcs * (5.0 / 1024.0);
     Voltage *= 10;
     Serial.println(Voltage);
-    
-    
+
+
     // Bereken de stroom in Ampère
     // 2.5 is de offset (bij 0A output is 2.5V)
     // 0.066V (66mV) is de stijging in uitgangsspanning bij 1A stroom
@@ -90,10 +100,10 @@ void loop() {
     // Serial.print(" V, Current: ");
     // Serial.print(AcsValueF);
     // Serial.println(" A"); // Druk de gemeten stroom af op de seriële monitor
-    
-    delay(100); // Wacht 100ms voor de volgende meting
 
-    float f = 123.45;  // Je hebt een float waarde
+    delay(100);  // Wacht 100ms voor de volgende meting
+
+    float f = 123.45;        // Je hebt een float waarde
     uint8_t u = (uint8_t)f;  // Je zet de float om naar uint8_t
     // Serial.println(u);  // Print de waarden
 
@@ -101,12 +111,12 @@ void loop() {
 
 
     // GEEN kommagetallen toegestaan!
-    uint16_t zonnepanelenStroom = (uint16_t)AcsValueF; // mA - milli ampère
-    uint8_t zonnepanelenSpanning = (uint8_t)Voltage; // V * 10 - volt vermenigvuldigd door 10
-    uint8_t zonnepanelenVoortgang = 76; // percentage van 0 t/m 100 van hoe ver de zonnepanelen zijn uitgeklapt
+    uint16_t zonnepanelenStroom = (uint16_t)AcsValueF;  // mA - milli ampère
+    uint8_t zonnepanelenSpanning = (uint8_t)Voltage;    // V * 10 - volt vermenigvuldigd door 10
+    uint8_t zonnepanelenVoortgang = 76;                 // percentage van 0 t/m 100 van hoe ver de zonnepanelen zijn uitgeklapt
 
-    uint16_t accuStroom = 1234; // mA - milli ampère
-    uint8_t accuSpanning = 93; // V * 10 - volt vermenigvuldigd door 10
+    uint16_t accuStroom = 1234;  // mA - milli ampère
+    uint8_t accuSpanning = 93;   // V * 10 - volt vermenigvuldigd door 10
 
 
 
@@ -117,7 +127,7 @@ void loop() {
 
     txData[cursor++] = zonnepanelenVoortgang;
 
-    
+
     txData[cursor++] = accuStroom >> 8;
     txData[cursor++] = accuStroom;
 
@@ -126,15 +136,15 @@ void loop() {
 
 
     // vult de overige plekjes van de 32 bytes met 0
-    while (cursor<RF24_PAYLOAD_SIZE) {
+    while (cursor < RF24_PAYLOAD_SIZE) {
       txData[cursor++] = 0;
     }
-        
-  /****************** Transmit Mode ***************************/
 
-   // Print transmit data in Hex format
+    /****************** Transmit Mode ***************************/
+
+    // Print transmit data in Hex format
     Serial.print("txData: ");
-    for (size_t i=0; i<cursor; ++i) {
+    for (size_t i = 0; i < cursor; ++i) {
       if (i != 0) Serial.print(" ");
       printHex2(txData[i]);
     }
@@ -154,14 +164,14 @@ void loop() {
 
   /****************** Receive Mode ***************************/
 
-  if (radio.available()) {      //'available' means whether valid bytes have been received and are waiting to be read from the receive buffer
+  if (radio.available()) {  //'available' means whether valid bytes have been received and are waiting to be read from the receive buffer
     // Receive data from radio
-    while (radio.available()) { // While there is data ready
+    while (radio.available()) {             // While there is data ready
       radio.read(&rxData, sizeof(rxData));  // Get the payload
     }
     // Print received data in Hex format
     Serial.print("rxData: ");
-    for (size_t i=0; i<RF24_PAYLOAD_SIZE; ++i) {
+    for (size_t i = 0; i < RF24_PAYLOAD_SIZE; ++i) {
       if (i != 0) Serial.print(" ");
       printHex2(rxData[i]);
     }
@@ -170,10 +180,26 @@ void loop() {
     // in of uitklappen
     if (bitRead(rxData[0], 0)) {
       Serial.println("uitklappen");
-    }
-    else if (bitRead(rxData[0], 1)) {
+      // Draai de motor 12 seconden vooruit
+      digitalWrite(dirPin, HIGH);  // Stel de draairichting in
+      runMotor(18000);             // Draai de motor 12 seconden
+    } else if (bitRead(rxData[0], 1)) {
       Serial.println("inklappen");
+      // Draai de motor 12 seconden achteruit
+      digitalWrite(dirPin, LOW);  // Verander de draairichting
+      runMotor(18000);            // Draai de motor 12 seconden
     }
   }
 
 }  // Loop
+
+void runMotor(unsigned long duration) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < duration) {
+    // Stuur een puls naar de step pin
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(stepDelay);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(stepDelay);
+  }
+}
